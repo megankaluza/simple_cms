a9ec6daa374354bf16e4d867019e6f9f93dd820a
Megan Kaluza <megankaluza@Megans-MacBook-Pro.local>
2018-01-28 12:24:45 -0800

    add controller filters/debugging logs

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,7 +2,11 @@ class SubjectsController < ApplicationController
 
   layout 'admin'
 
+  before_action :find_subjects, :only => [:new, :create, :edit, :update]
+  before_action :set_subject_count, :only => [:new, :create, :edit, :update]
+
   def index
+    logger.debug("**** Testing the logger. ****")
     @subjects = Subject.sorted
   end
 
@@ -12,7 +16,7 @@ class SubjectsController < ApplicationController
 
   def new
     @subject = Subject.new(:name => 'Default')
-    @subject_count = Subject.count + 1
+    # @subject_count = Subject.count + 1
   end
 
   def create
@@ -25,7 +29,7 @@ class SubjectsController < ApplicationController
      redirect_to(subjects_path)
    else
     # If save fails, redisplay the form so user can fix problems
-    @subject_count = Subject.count + 1
+    # @subject_count = Subject.count + 1
     render('new')
   end
 
@@ -33,7 +37,7 @@ end
 
   def edit
     @subject = Subject.find(params[:id])
-    @subject_count = Subject.count
+    # @subject_count = Subject.count
   end
 
   def update
@@ -46,7 +50,7 @@ end
      redirect_to(subject_path(@subject))
    else
     # If save fails, redisplay the form so user can fix problems
-    @subject_count = Subject.count
+    # @subject_count = Subject.count
     render('edit')
   end
 
@@ -67,6 +71,17 @@ end
 
   def subject_params
     params.require(:subject).permit(:name, :position, :visible, :created_at)
+  end
+
+  def find_subjects
+    @subjects = Subject.sorted
+  end
+
+  def set_subject_count
+    @subject_count = Subject.count
+    if params[:action] == 'new' || params[:action] == 'create'
+      @subject_count += 1
+    end
   end
 
 end
